057bd17163a5073c3dd069cea235876a7544823c
José <Jose.Paumard@gmail.com>
2018-02-20 11:05:13 +0100

    Updated the toString method

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -13,16 +13,16 @@ public class User {
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
-
 	public int getAge() {
 		return age;
 	}
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
 	}
 }
